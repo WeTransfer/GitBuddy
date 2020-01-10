3dbf7c3528b28cb6e08d6513591d62c3d72d171b
Antoine van der Lee <antoine@wetransfer.com>
2020-01-10 13:28:32 +0100

    Created a start for Git commands

--- Sources/ChangelogProducerCore/ChangelogProducer.swift
+++ Sources/ChangelogProducerCore/ChangelogProducer.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Shell
 
 public final class ChangelogProducer {
     private let arguments: [String]
@@ -17,7 +18,17 @@ public final class ChangelogProducer {
     /// Input parameters:
     /// - Target branch, defaults to master
     public func run() throws {
-        print("Hello world")
+        let shell = Shell()
+
+        print("Fetching tags")
+        _ = shell.sync(["git", "fetch", "--tags"])
+
+        // git tag --sort=committerdate | tail -1
+        let latestTag = try shell.capture(["git", "tag", "--sort=committerdate", "| tail -1"]).get()
+        print("Latest tag is \(latestTag)")
+        let tagCreationDate = try Shell().capture(["git", "log", "-1", "--format=%ai", latestTag]).get()
+        print("Tag \(latestTag) is created at \(tagCreationDate)")
+
         /// Fetch last release from GitHub
         /// Fetch PRs after last released tag creation date
     }
