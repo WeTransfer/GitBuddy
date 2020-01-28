0ee2134f3b8b937a2bfcbd75cf80b457ca29ced3
Antoine van der Lee <ajvanderlee@gmail.com>
2020-01-28 14:50:48 +0100

    Remove unneeded // dev

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"])
     ],
     dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"), // dev
-        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift"), // dev
+        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
+        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift"),
         // dev .package(url: "https://github.com/WeTransfer/Mocker.git", from: "2.0.0"),
         .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.9.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0")
