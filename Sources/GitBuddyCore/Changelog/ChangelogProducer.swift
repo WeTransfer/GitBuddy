51bd407e990a2f5ce9bd5c43457992fc72e2f5fd
Antoine van der Lee <ajvanderlee@gmail.com>
2020-02-06 15:32:35 +0100

    Comment on related issues when the release command is triggered (#15)
    * Related issues and pull requests are now commented
    
    * Changelog is now tested
    
    * Add tests for commenting
    
    * Improve the structure for posting comments
    
    * Fix Octokit reference
    
    * Updated readme to include all the new commands (#16)
    
    * Update the readme to reflect the current changes
    
    * Update README.md
    
    Co-Authored-By: Kaira <kaira@wetransfer.com>
    
    * Update README.md
    
    Co-Authored-By: Kaira <kaira@wetransfer.com>
    
    Co-authored-by: Kaira <diagnekaira@yahoo.com>
    
    Co-authored-by: Kaira <diagnekaira@yahoo.com>

--- Sources/GitBuddyCore/Changelog/ChangelogProducer.swift
+++ Sources/GitBuddyCore/Changelog/ChangelogProducer.swift
@@ -18,15 +18,17 @@ struct ChangelogCommand: Command {
 
     init(parser: ArgumentParser) {
         let subparser = parser.add(subparser: command, overview: description)
-        sinceTag = subparser.add(option: "--sinceTag", shortName: "-s", kind: String.self, usage: "The tag to use as a base")
-        baseBranch = subparser.add(option: "--baseBranch", shortName: "-b", kind: String.self, usage: "The base branch to compare with")
+        sinceTag = subparser.add(option: "--sinceTag", shortName: "-s", kind: String.self, usage: "The tag to use as a base. Defaults to the latest tag.")
+        baseBranch = subparser.add(option: "--baseBranch", shortName: "-b", kind: String.self, usage: "The base branch to compare with. Defaults to master.")
         _ = subparser.add(option: "--verbose", kind: Bool.self, usage: "Show extra logging for debugging purposes")
     }
 
     @discardableResult func run(using arguments: ArgumentParser.Result) throws -> String {
         let changelogProducer = try ChangelogProducer(sinceTag: arguments.get(sinceTag),
                                                       baseBranch: arguments.get(baseBranch))
-        return try changelogProducer.run()
+
+        Log.debug("Result of creating the changelog:\n")
+        return try changelogProducer.run().description
     }
 }
 
@@ -35,27 +37,25 @@ final class ChangelogProducer: URLSessionInjectable {
 
     private lazy var octoKit: Octokit = Octokit()
     let base: Branch
-    let baseRelease: Release
+    let baseTag: Tag
     let project: GITProject
 
     init(sinceTag: String?, baseBranch: Branch?) throws {
         if let tag = sinceTag {
-            baseRelease = try Release(tag: tag)
+            baseTag = try Tag(name: tag)
         } else {
-            baseRelease = try Release.latest()
+            baseTag = try Tag.latest()
         }
         base = baseBranch ?? "master"
         project = GITProject.current()
     }
 
-    @discardableResult public func run() throws -> String {
-        Log.debug("Getting all changes happened after \(baseRelease.tag)")
+    @discardableResult public func run() throws -> Changelog {
+        Log.debug("Getting all changes happened after \(baseTag.name)")
 
         let pullRequestsFetcher = PullRequestFetcher(octoKit: octoKit, base: base, project: project)
-        let pullRequests = try pullRequestsFetcher.fetchAllAfter(baseRelease, using: urlSession)
+        let pullRequests = try pullRequestsFetcher.fetchAllAfter(baseTag, using: urlSession)
         let items = ChangelogItemsFactory(octoKit: octoKit, pullRequests: pullRequests, project: project).items(using: urlSession)
-        let changelog = ChangelogBuilder(items: items).build()
-
-        return changelog
+        return Changelog(items: items)
     }
 }
---
+++ Tests/GitBuddyTests/Models/ChangelogTests.swift
@@ -0,0 +1,54 @@
+//
+//  ChangelogTests.swift
+//  GitBuddyTests
+//
+//  Created by Antoine van der Lee on 05/02/2020.
+//
+
+import Foundation
+import XCTest
+@testable import GitBuddyCore
+
+final class ChangelogTests: XCTestCase {
+
+    /// It should report a single issue from a pull request correctly.
+    func testPullRequestSingleIssue() {
+        let pullRequest = MockedPullRequest(number: 0)
+        let issue = MockedIssue(number: 0)
+        let changelogItem = ChangelogItem(input: issue, closedBy: pullRequest)
+        let changelog = Changelog(items: [changelogItem])
+
+        XCTAssertEqual(changelog.itemIdentifiers, [0: [0]])
+    }
+
+    /// It should report multiple issues from a single pull request correctly.
+    func testPullRequestMultipleIssues() {
+        let pullRequest = MockedPullRequest(number: 0)
+        let changelogItems = [
+            ChangelogItem(input: MockedIssue(number: 0), closedBy: pullRequest),
+            ChangelogItem(input: MockedIssue(number: 1), closedBy: pullRequest)
+        ]
+        let changelog = Changelog(items: changelogItems)
+
+        XCTAssertEqual(changelog.itemIdentifiers, [0: [0, 1]])
+    }
+
+    /// It should report the pull request even though there's no linked issues.
+    func testPullRequestNoIssues() {
+        let pullRequest = MockedPullRequest(number: 0)
+        let changelogItem = ChangelogItem(input: pullRequest, closedBy: pullRequest)
+        let changelog = Changelog(items: [changelogItem])
+
+        XCTAssertEqual(changelog.itemIdentifiers, [0: []])
+    }
+
+    /// It should create a changelog from its inputs.
+    func testDescription() {
+        let pullRequest = MockedPullRequest(number: 0)
+        let issue = MockedIssue(title: "Fixed something")
+        let changelogItem = ChangelogItem(input: issue, closedBy: pullRequest)
+        let changelog = Changelog(items: [changelogItem])
+
+        XCTAssertEqual(changelog.description, "- Fixed something")
+    }
+}
