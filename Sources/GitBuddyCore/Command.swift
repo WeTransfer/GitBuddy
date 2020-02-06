80febcba429264a2e744686e67d6091f1231e317
Antoine van der Lee <ajvanderlee@gmail.com>
2020-02-06 13:46:29 +0100

    Add documentation to the Command class

--- Sources/GitBuddyCore/Command.swift
+++ Sources/GitBuddyCore/Command.swift
@@ -16,6 +16,13 @@ protocol Command {
     /// The command description explaining what it does.
     var description: String { get }
 
+    /// Creates a new instance of this `Command`.
+    /// - Parameter parser: The main parser that is being used for execution.
     init(parser: ArgumentParser) throws
+
+    /// Runs the command with the given arguments and environment.
+    /// - Parameters:
+    ///   - arguments: The arguments that are parsed upon execution.
+    ///   - environment: The available environment variables.
     @discardableResult func run(using arguments: ArgumentParser.Result, environment: [String: String]) throws -> String
 }
