a77ab9c35ab930ec9438e7501377f9783134f8ce
Max Desiatov <max@desiatov.com>
2020-04-01 10:26:01 +0100

    Fix SPMUtility version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift"),
         // dev .package(url: "https://github.com/WeTransfer/Mocker.git", from: "2.1.0"),
         .package(url: "https://github.com/nerdishbynature/octokit.swift", .upToNextMajor(from: "0.10.1")),
-        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0")
+        .package(url: "https://github.com/apple/swift-package-manager.git", .upToNextMinor(from: "0.4.0"))
     ],
     targets: [
         // dev .testTarget(name: "GitBuddyTests", dependencies: ["GitBuddy", "Mocker"]),
