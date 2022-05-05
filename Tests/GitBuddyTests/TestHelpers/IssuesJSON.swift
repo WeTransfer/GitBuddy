b07f38613032b78156be87802d3895c6cb8d29ce
Antoine van der Lee <ajvanderlee@gmail.com>
2022-05-05 11:43:20 +0200

    Also add an emoji to the issue example

--- Tests/GitBuddyTests/TestHelpers/IssuesJSON.swift
+++ Tests/GitBuddyTests/TestHelpers/IssuesJSON.swift
@@ -777,7 +777,7 @@ let IssuesJSON = #"""
     "id": 545013288,
     "node_id": "MDExOlB1bGxSZXF1ZXN0MzU5MDA1MjAz",
     "number": 50,
-    "title": "Add charset utf-8 to html head",
+    "title": "Add charset utf-8 to html head 🫥",
     "user": {
       "login": "intitni",
       "id": 793147,
